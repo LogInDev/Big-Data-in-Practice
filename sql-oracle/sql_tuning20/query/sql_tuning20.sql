584fc0388a4777a5402e39eec23655ade20d0bb0
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-04-10 14:22:27 +0900

    chapter 2. 인덱스의 구조

--- sql-oracle/sql_tuning20/query/sql_tuning20.sql
+++ sql-oracle/sql_tuning20/query/sql_tuning20.sql
@@ -48,3 +48,61 @@ where empno = 7788;
 select * from table(dbms_xplan.display_cursor(null,null,'ALLSTATS LAST'));
 
 select index emp_empno on emp(empno);
+
+--2. 인덱스의 구조
+
+-- 예제1. 월급에 인덱스를 생성하고 인덱스의 구조를 확인해라.
+
+create index emp_sal
+on emp(sal);
+
+select sal, rowid
+from emp
+where sal >= 0;
+
+--예제2. 이름에 인덱스를 생성하고 인덱스의 구조
+create index emp_ename
+on emp(ename);
+
+select ename, rowid
+from emp
+where ename > ' ';
+
+select /* gather_plan_statistics */ ename, rowid
+from emp
+where ename > ' ';
+
+select * from table(dbms_xplan.display_cursor(null,null, 'ALLSTATS LAST'));
+
+select /* gather_plan_statistics */ ename, rowid
+from emp;
+
+select * from table(dbms_xplan.display_cursor(null,null, 'ALLSTATS LAST'));
+
+--예제3. 입사일에 인덱스를 생성하고 인덱스의 구조를 확인해라.
+
+create index emp_hiredate
+on emp(hiredate);
+
+select /*+ gather_plan_statistics */ hiredate, rowid
+from emp
+where hiredate < to_date('9999/12/31', 'RRRR/MM/DD');
+
+select * from table(dbms_xplan.display_cursor(null,null, 'ALLSTATS LAST'));
+
+select /*+ gather_plan_statistics */ hiredate, rowid
+from emp;
+
+select * from table(dbms_xplan.display_cursor(null,null, 'ALLSTATS LAST'));
+
+-- dept 테이블에 loc에 인덱스를 생성하고 인덱스의 구조를 확인해라.
+create index dept_loc
+on dept(loc);
+
+select /*+ gather_plan_statistics */ loc, rowid
+from dept
+where loc> ' ';
+
+select * from table(dbms_xplan.display_cursor(null,null, 'ALLSTATS LAST'));
+
+
